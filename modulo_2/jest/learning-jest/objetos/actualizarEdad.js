73cddccafd9d446d053042beb1816aac5d9fcbf6
Adavidfr <adavidfruiz@gmail.com>
2025-10-31 10:11:19 -0500

    feat: 10/31/2025

---
+++ modulo_2/jest/learning-jest/objetos/actualizarEdad.js
@@ -0,0 +1,14 @@
+function actualizarEdad(persona, edad) {
+    if (typeof persona !== 'object') {
+        throw new TypeError('persona invalida');
+    }
+
+    if (!Number.isInteger(edad) || edad < 0) {
+        throw new TypeError('edad invalida');
+    }
+
+    persona.edad = edad;
+    return persona;
+}
+
+module.exports = { actualizarEdad };
